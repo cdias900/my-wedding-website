45afdedc883b6fecba57d7fa6b35dcea7a88bcb5
Pedro Dias <pedro@cdias.dev>
2022-08-06 19:46:02 -0300

    fix: remove event tracking from development

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -3,6 +3,8 @@ import { getAnalytics, logEvent } from 'firebase/analytics';
 import { app } from 'config/analytics';
 
 export const trackEvent = (event: string, params?: { [key: string]: any }) => {
+  if (process.env.NODE_ENV === 'development') return;
+
   const analytics = getAnalytics(app);
   logEvent(analytics, event, params);
 };
