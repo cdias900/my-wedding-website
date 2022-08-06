950c7ad473893f8172eab3549eacd8db9177861a
Pedro Dias <pedro@cdias.dev>
2022-08-06 18:13:46 -0300

    chore: add casa comfort gift list

--- src/pages/GiftList/constants.ts
+++ src/pages/GiftList/constants.ts
@@ -1,5 +1,6 @@
 import HavanLogo from 'assets/images/gift-list/havan-logo.png';
 import DularLogo from 'assets/images/gift-list/dular-logo.png';
+import CasaComfortLogo from 'assets/images/gift-list/casa-comfort-logo.png';
 
 export const giftLists = [
   {
@@ -11,5 +12,10 @@ export const giftLists = [
     title: 'Dular',
     url: 'https://www.listasdular.com.br/listas/listaProdutos.jsp?lista.codLista=75155',
     logo: DularLogo,
+  },
+  {
+    title: 'Casa Comfort',
+    url: 'https://bit.ly/3khnzlZ',
+    logo: CasaComfortLogo,
   },
 ];
